9b087b3e180bbc99f46f4fe72ff9e5e284b5206a
Kevin Stokes <kevinstokes@Kevins-MBP.attlocal.net>
2019-05-26 19:15:00 -0400

    added complete file structure

--- config/orm.js
+++ config/orm.js
@@ -2,12 +2,13 @@ var connection = require('./connection.js');
 
 var orm = {
     selectAll: function(whatToSelect, tableInput) {
-      var queryString = "SELECT ?? FROM ??";
+      var queryString = "SELECT * FROM ??";
       connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
         if (err) throw err;
         console.log(result);
       });
     },
+
     insertOne: function(tableInput, colToSearch, valOfCol) {
       var queryString = "SELECT * FROM ?? WHERE ?? = ?";
   
