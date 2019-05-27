d6a09f91cee8a00c59978b7b7c923065fd3f5da8
Kevin Stokes <kevinstokes@Kevins-MBP.attlocal.net>
2019-05-26 20:36:01 -0400

    routes working and displaying

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,9 @@
 var express = require('express');
+var router = express.Router();
 var burger = require ('../models/burger.js');
+
+router.get('/', function (req, res) {
+    res.render('index');
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
 // Serve statis content from the public directory in the application directory
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 // Lets Express handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,9 +17,6 @@ var exphbs = require('express-handlebars');
 app.engine("handlebars", exphbs({defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// End of requiring Express Handlebars
-
-
 // Serve index.handlebars to the root route.
 // app.get('/', function (req, res) {
 //     res.send('Hello World')
@@ -28,7 +27,6 @@ app.set("view engine", "handlebars");
 // });
 
 // Start server so it listens for client requests
-
 app.listen(PORT, function() {
     console.log("Server listening on http://localhost:" + PORT);
 });
