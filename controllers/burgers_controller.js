6cad040881013c29827b8d4d3b313d70836d4d39
Kevin Stokes <kevinstokes@kevins-mbp.attlocal.net>
2019-05-28 21:18:22 -0400

    burgers displaying on page from DB

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
     burger.selectAll(function(burger_data){
         console.log(burger_data);
         console.log("Made it here");
-        res.render('index');
+        res.render('index',{burger_data});
     })
     
 });
